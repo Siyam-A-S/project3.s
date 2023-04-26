fe1a3e0be77805164e4d8c11029fb51efb3eab01
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2023-04-26 01:14:50 -0400

    Update project3.s
    Added some more necessary labels

--- project3.s
+++ project3.s
@@ -1,4 +1,7 @@
 .data
 	userInput: .space 1001			# to store user input (1000 characters maximum)
 	invalid: .asciiz "X"			# label to store invalid input message
+	comma: .asciiz ","			# label to store comma
+	left_paren: .asciiz "("			# label to store left paremthesis
+	right_paren: .asciiz ")"		# label to store rught parenthesis
 .text
