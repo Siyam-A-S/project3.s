04ef3ca939070908199fbe1bfe4e4e551d862fb4
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2023-04-26 01:11:59 -0400

    Update project3.s

--- project3.s
+++ project3.s
@@ -1,4 +1,4 @@
 .data
 	userInput: .space 1001			# to store user input (1000 characters maximum)
-	invalid: .asciiz "Invalid input"	# label to store invalid input message
+	invalid: .asciiz "X"			# label to store invalid input message
 .text
