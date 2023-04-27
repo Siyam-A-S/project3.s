b27db78e76e54bad0fdf4dbd3bd84624777fec59
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2023-04-27 12:59:23 -0400

    Update project3.s
    Comments on finding the value of N. Prompt the user for input.

--- project3.s
+++ project3.s
@@ -5,3 +5,16 @@
 	left_paren: .asciiz "("			# label to store left paremthesis
 	right_paren: .asciiz ")"		# label to store rught parenthesis
 .text
+
+main:	
+	# calculate N and M
+	# My Howard ID: 03043178
+	# N = 26 + (3043178 % 11) = 32
+	# M = 32-10 = 22
+	# Range: 0-9, a-v, A-V
+
+	# prompt user for input
+	li $v0, 8 				# read string system call
+	la $a0, userInput 			# load input buffer
+	li $a1, 1001 				# maximum length (including null terminator) 
+	syscall 				# execute
